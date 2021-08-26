e6399999b23727f16e6ab3bb0682391f66e28a64
Jeremy Cady <jcady90@icloud.com>
2021-08-26 14:22:08 -0500

    initial

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import logger from 'redux-logger';
+import createSagaMiddleware from '@redux-saga/core';
+import { put, takeEvery } from '@redux-saga/core/effects';
+import axios from 'axios';
+
+function* rootSaga() {
+    
+}
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+    combineReducers({}),
+    applyMiddleware(logger, sagaMiddleware)
+
+)
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(<App />, document.getElementById('root'));
