7e464804bdca30c86e1f8ff1302e80bebd13b001
Eben Stine <ebenstine@gmail.com>
2021-08-26 14:01:30 -0500

    search component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { takeEvery, put } from 'redux-saga/effects';
+import axios from 'axios';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const sagaMiddleware = createSagaMiddleware();
+
+const reduxStore = createStore(
+    combineReducers({
+        
+    }),
+    // Plug saga middleware into redux
+    applyMiddleware(sagaMiddleware, logger)
+  );
+
+  function* watcherSaga() {
+
+  };
+
+  sagaMiddleware.run(watcherSaga);
+
+ReactDOM.render(<Provider store={reduxStore}><App /></Provider>, document.getElementById('root'));
