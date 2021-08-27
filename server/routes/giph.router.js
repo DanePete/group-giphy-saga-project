c532c12fb956eb2297fcfaf1f4f29268c46d2e57
Jeremy Cady <jcady90@icloud.com>
2021-08-27 09:59:24 -0500

    saving before pulling and merging

--- server/routes/giph.router.js
+++ server/routes/giph.router.js
@@ -7,7 +7,7 @@ router.get('/search/:string', (req, res) => {
   // return all categories
  axios({
      method: 'GET',
-     url: "https://api.giphy.com/v1/gifs/search"
+     url: "https://api.giphy.com/v1/gifs/search",
      params: { 
          api_key: process.env.GIPHY_API_KEY,
          q: req.params.string
