eb6d0e0c1a197ffa6af65afafc3772a540c035c3
dane <dane.m.petersen@gmail.com>
2021-08-27 10:26:43 -0500

    fixed mapping issue, we needed to handle an empty array

--- src/components/GiphList/GiphList.js
+++ src/components/GiphList/GiphList.js
@@ -12,12 +12,22 @@ let exampleData = [{url:"https://giphy.com/gifs/cheeseburger-burger-king-food-sE
 
 return (
     <>
-    
+    <h1>GOT HERE YO</h1>
     <ul>
-        
-        {giphList.data.map((search, index) =>
-        <li key={index}>{search.url}</li> 
-        )}
+        {giphList.data?.map((search, index) => {
+
+
+            // REPLACE CURRENT RETURN WITH COMMENTED Out RETURN ONCE WE HAVE THE INDIVIDuAL COMPONENT  
+
+            // return (
+            //     <ListItem 
+            //         data={search}
+            //     />
+            // )
+            return (
+                <li key={index}>{search.url}</li> 
+            )
+        })}
     </ul>
     </>
     
